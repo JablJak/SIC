f89d3c7792e8a9e4bb7ce4c7bc40ef374ef56b6d
Jakub Jabłoński <jakub.jablonski8.stud@pw.edu.pl>
2025-03-04 22:34:01 +0100

    Train SWIN-T-IC_v0.2.1

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -1,6 +1,8 @@
 import unittest
 
-from src.utils.const import PROJECT_ROOT
+from clearml import InputModel
+
+from src.utils.const import PROJECT_ROOT, EXPERIMENTS_CONFIG_PATH
 from src.utils.initializers import read_config, dataset_from_config, model_from_config, dataloader_from_config, \
     optimizer_from_config, scheduler_from_config
 
@@ -11,13 +13,18 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
-        example_config = read_config(f"{PROJECT_ROOT}/configs/experiments/example_experiment.yaml")
+        example_config = read_config(f"{EXPERIMENTS_CONFIG_PATH}/example_experiment.yaml")
         model_config = example_config['model']
         dataset_config = example_config['dataset']
         dataloader_config = example_config['dataloader']
         optimizer_config = example_config['optimizer']
         scheduler_config = example_config['scheduler']
 
+        model1 = InputModel.import_model(
+            name='Cokolwiek',
+            weights_url='file:///run/media/jakub/Dane/Studia/INZ/models/SWIN-T-IC_0.2.0.pth',
+            framework='PyTorch'
+        )
         model = model_from_config(model_config)
         dataset = dataset_from_config(dataset_config)
         dataloader = dataloader_from_config(dataset, dataloader_config)
