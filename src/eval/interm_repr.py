0cd9c332541b84eacbcbbd49207208be019874a2
Jakub Jabłoński <jakub.jablonski8.stud@pw.edu.pl>
2025-03-07 21:28:35 +0100

    ClearML upload is optional

--- src/eval/interm_repr.py
+++ src/eval/interm_repr.py
@@ -1,5 +1,11 @@
 class IntermediateRepresentation:
     def __init__(self):
+        self.single_input_size = None
+        self.input_size = None
+        self.input_num_el = None
+        self.input_element_size = None
+        self.input_shape = None
+        self.input_values = None
         self.single_output_size = None
         self.values = None
         self.shape = None
@@ -15,6 +21,21 @@ class IntermediateRepresentation:
         self.size = self.element_size * self.num_el
         self.single_output_size = self.size // self.shape[0]
 
+        self.input_values = input[0].detach().cpu().numpy()
+        self.input_shape = input[0].shape
+        self.input_element_size = input[0].element_size()
+        self.input_num_el = input[0].numel()
+        self.input_size = self.input_element_size * self.input_num_el
+        self.single_input_size = self.input_size // self.input_shape[0]
+
     def __str__(self):
-        return (f"IntermediateRepresentation(shape={self.shape}, size={self.size}, "
-                f"single_output_size={self.single_output_size})")
+        return (f"IntermediateRepresentation(single_input_size={self.single_input_size}, "
+                f"input_size={self.input_size}, "
+                f"input_num_el={self.input_num_el}, "
+                f"input_element_size={self.input_element_size}, "
+                f"input_shape={self.input_shape}, "
+                f"single_output_size={self.single_output_size}, "
+                f"shape={self.shape}, "
+                f"element_size={self.element_size}, "
+                f"num_el={self.num_el}, "
+                f"size={self.size}")
---
+++ src/utils/clearml_helpers.py
@@ -0,0 +1,19 @@
+from clearml import OutputModel, Task, Logger
+from clearml.task import TaskInstance
+
+from src.train.experiment import Experiment
+
+
+def save_model(task: TaskInstance, experiment: Experiment, weights_file_path: str):
+    output_model = OutputModel(task=task, name=experiment.output_model_name())
+    output_model.update_weights(weights_file_path)
+
+    model_id = output_model.id
+    print(f"Saved ClearML model with ID: {model_id}")
+
+def start_experiment(experiment: Experiment) -> tuple[TaskInstance, Logger]:
+    task = Task.init(project_name=experiment.project, task_name=experiment.task)
+    task.connect(experiment.config)
+    task.set_comment(experiment.comment)
+    logger = task.get_logger()
+    return task, logger
