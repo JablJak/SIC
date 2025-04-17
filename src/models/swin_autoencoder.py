91809342f7ba9a59319f7c76a5327d803a9884be
Jakub Jabłoński <jakub.jablonski8.stud@pw.edu.pl>
2025-04-17 11:51:52 +0200

    Train SWIN-S-IC_v0.24.0
    - Fixed activation (min 1e-6)
    - Decrease kernel size in patch reconstruction

--- src/models/swin_autoencoder.py
+++ src/models/swin_autoencoder.py
@@ -16,16 +16,12 @@ from src.utils.activation import LearnableTempSigmoid
 class PatchReconstruction(nn.Module):
     def __init__(self, dim):
         super().__init__()
-        self.conv_trans1 = nn.ConvTranspose2d(dim, dim, kernel_size=3, stride=1, padding=1)
-        self.conv_trans2 = nn.ConvTranspose2d(dim, 3, kernel_size=3, stride=1, padding=1)
-        self.relu = nn.LeakyReLU()
+        self.conv_trans = nn.ConvTranspose2d(dim, 3, kernel_size=2, stride=2, padding=0)
         self.activation = LearnableTempSigmoid()
 
     def forward(self, x):
         x = x.permute(0, 3, 1, 2)
-        x = self.conv_trans1(x)
-        x = self.relu(x)
-        x = self.conv_trans2(x)
+        x = self.conv_trans(x)
         x = self.activation(x)
         return x
 
