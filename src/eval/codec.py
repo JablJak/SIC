97b4241ec560029816cb09e220c1857af7088e89
Jakub Jabłoński <jakub.jablonski8.stud@pw.edu.pl>
2026-01-10 16:58:22 +0100

    Cleanup and improve eval viz

--- src/eval/codec.py
+++ src/eval/codec.py
@@ -1,12 +1,10 @@
-import json
 import os
 import subprocess
-from abc import ABC, abstractmethod
-from datetime import timezone, datetime
+from abc import ABC
+from datetime import datetime
 from typing import Iterable
 
 import PIL.Image
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from io import BytesIO
@@ -21,21 +19,23 @@ from torchvision.transforms.v2.functional import to_pil_image, to_image, to_dtyp
 from src.data.base_dataset import BaseDataset
 from src.data.kodak_dataset import KodakDataset
 from src.data.transforms import RGBCompression, RGB_MIXED_LIC_MEAN, RGB_MIXED_LIC_STD
-from src.utils.const import ARTIFACTS_PATH
+
 from src.utils.initializers import model_from_config
 
+pil_test_dataset = KodakDataset(None, None, None, None)
+swin_test_dataset = KodakDataset(RGBCompression(noresize=True, normalize=True, mean=RGB_MIXED_LIC_MEAN, std=RGB_MIXED_LIC_STD)
+                                 ,None, None, None)
+class Codec():
+    def __init__(self, qualities = None, label = None, bpp = None, psnr = None, msssim = None, type = None):
+        self.qualities: Iterable | None = qualities
+        self.label: str | None = label
+        self.bpp = [] if bpp is None else bpp
+        self.psnr = [] if psnr is None else psnr
+        self.msssim = [] if msssim is None else msssim
+        self.type = type
 
-class Codec(ABC):
-    def __init__(self):
-        self.qualities: Iterable | None = None
-        self.label: str | None = None
-        self.bpp = []
-        self.psnr = []
-        self.msssim = []
-
-    @abstractmethod
     def encode(self, ref_img: PIL.Image.Image, quality: int | float, **kwargs) -> tuple[ImageFile, float]:
-        pass
+        raise NotImplementedError
 
     def eval_codec(self, dataset: BaseDataset, out_dir: str = None):
         for quality in self.qualities:
@@ -250,62 +250,3 @@ class AVIFCodec(PILCodec):
     def encode(self, ref_img: PIL.Image.Image, quality: int, **kwargs) -> tuple[ImageFile, float]:
         return super().encode(ref_img=ref_img, quality=quality, speed=0, format='AVIF', **kwargs)
 
-
-jpeg_codec = JPEGCodec()
-jpeg2000_codec = JPEG2000Codec()
-webp_codec = WebPCodec()
-avif_codec = AVIFCodec()
-bpg_codec = BPGCodec()
-swin_lic_codec = SwinLicCodec()
-
-codecs = [
-    jpeg_codec,
-    jpeg2000_codec,
-    webp_codec,
-    avif_codec,
-    bpg_codec,
-    swin_lic_codec,
-]
-
-pil_test_dataset = KodakDataset(None, None, None, None)
-swin_test_dataset = KodakDataset(RGBCompression(noresize=True, normalize=True, mean=RGB_MIXED_LIC_MEAN, std=RGB_MIXED_LIC_STD)
-                                 ,None, None, None)
-
-artifacts_dir = ARTIFACTS_PATH / (datetime.now(timezone.utc).strftime("%Y%m%dT%H%M") + "-14")
-artifacts_dir.mkdir(parents=True, exist_ok=True)
-
-px = 1/plt.rcParams['figure.dpi']  # pixel in inches
-plt.figure(figsize=(900*px, 600*px))
-for codec in codecs:
-    if isinstance(codec, (PILCodec, BPGCodec)):
-        codec.eval_codec(pil_test_dataset, out_dir=artifacts_dir)
-    if isinstance(codec, SwinLicCodec):
-        codec.eval_codec(swin_test_dataset, out_dir=artifacts_dir)
-    plt.plot(codec.bpp, codec.psnr, label=codec.label, marker='o')
-
-plt.grid(True)
-plt.legend()
-plt.xlim(0, 1.2)
-plt.ylim(20, 40)
-plt.yticks(range(20, 40, 2))
-plt.xlabel('Szybkość bitowa [bpp]')
-plt.ylabel('PSNR [dB]')
-plt.margins(0, 0)
-plt.tight_layout()
-plt.savefig(artifacts_dir / "rd_psnr.png")
-
-plt.figure(figsize=(900*px, 600*px))
-for codec in codecs:
-    plt.plot(codec.bpp, codec.msssim, label=codec.label, marker='o')
-
-plt.grid(True)
-plt.legend()
-plt.xlim(0, 1.2)
-plt.xlabel('Szybkość bitowa [bpp]')
-plt.ylabel('MS-SSIM')
-plt.margins(0, 0)
-plt.tight_layout()
-plt.savefig(artifacts_dir / "rd_ms_ssim.png")
-
-with open(artifacts_dir / "data.json", "w", encoding="utf-8") as f:
-    json.dump(codecs, f, indent=4, default=lambda o: o.__dict__)
---
+++ src/eval/eval_codecs.py
@@ -0,0 +1,70 @@
+import json
+from datetime import timezone, datetime
+
+import matplotlib.pyplot as plt
+
+from src.data.kodak_dataset import KodakDataset
+from src.data.transforms import RGBCompression, RGB_MIXED_LIC_MEAN, RGB_MIXED_LIC_STD
+from src.eval.codec import JPEGCodec, JPEG2000Codec, WebPCodec, AVIFCodec, SwinLicCodec, BPGCodec, PILCodec
+from src.utils.const import ARTIFACTS_PATH
+
+
+
+jpeg_codec = JPEGCodec()
+jpeg2000_codec = JPEG2000Codec()
+webp_codec = WebPCodec()
+avif_codec = AVIFCodec()
+bpg_codec = BPGCodec()
+swin_lic_codec = SwinLicCodec()
+
+codecs = [
+    jpeg_codec,
+    jpeg2000_codec,
+    webp_codec,
+    avif_codec,
+    bpg_codec,
+    swin_lic_codec,
+]
+
+pil_test_dataset = KodakDataset(None, None, None, None)
+swin_test_dataset = KodakDataset(RGBCompression(noresize=True, normalize=True, mean=RGB_MIXED_LIC_MEAN, std=RGB_MIXED_LIC_STD)
+                                 ,None, None, None)
+
+artifacts_dir = ARTIFACTS_PATH / (datetime.now(timezone.utc).strftime("%Y%m%dT%H%M") + "-14")
+artifacts_dir.mkdir(parents=True, exist_ok=True)
+
+px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+plt.figure(figsize=(900*px, 600*px))
+for codec in codecs:
+    if isinstance(codec, (PILCodec, BPGCodec)):
+        codec.eval_codec(pil_test_dataset, out_dir=artifacts_dir)
+    if isinstance(codec, SwinLicCodec):
+        codec.eval_codec(swin_test_dataset, out_dir=artifacts_dir)
+    plt.plot(codec.bpp, codec.psnr, label=codec.label, marker='o')
+
+plt.grid(True)
+plt.legend()
+plt.xlim(0, 1.2)
+plt.ylim(20, 40)
+plt.yticks(range(20, 40, 2))
+plt.xlabel('Szybkość bitowa [bpp]')
+plt.ylabel('PSNR [dB]')
+plt.margins(0, 0)
+plt.tight_layout()
+plt.savefig(artifacts_dir / "rd_psnr.png")
+
+plt.figure(figsize=(900*px, 600*px))
+for codec in codecs:
+    plt.plot(codec.bpp, codec.msssim, label=codec.label, marker='o')
+
+plt.grid(True)
+plt.legend()
+plt.xlim(0, 1.2)
+plt.xlabel('Szybkość bitowa [bpp]')
+plt.ylabel('MS-SSIM')
+plt.margins(0, 0)
+plt.tight_layout()
+plt.savefig(artifacts_dir / "rd_ms_ssim.png")
+
+with open(artifacts_dir / "data.json", "w", encoding="utf-8") as f:
+    json.dump(codecs, f, indent=4, default=lambda o: o.__dict__)
